102f5430e15386d3a23799fafe4efff0bc996a86
Felipe-HVSilva <felype.hen@hotmail.com>
2023-11-13 18:54:11 -0300

    feat: fetch to api countries

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>Home</h1>
+type Country = {
+  name: {
+    common: string
+  }
+}
+
+async function getCountries(): Promise<Country[]> {
+  const response = await fetch("https:restcountries.com/v3.1/all")
+  return response.json()
+}
+
+export default async function Home() {
+  const countries = await getCountries()
+  console.log(countries)
+  return (
+    <section className=" flex w-full container">
+      {countries.map((country) => (
+        <h1 key={country.name.common}>{country.name.common}</h1>
+      ))}
+    </section>
+  )
 }
