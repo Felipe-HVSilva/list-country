d7488649fc6981fc5ccb8ff182aa228233546187
Felipe-HVSilva <felype.hen@hotmail.com>
2023-11-15 12:04:53 -0300

    feat: add page Error

---
+++ app/pais/[name]/error.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import Link from "next/link"
+
+export default function Erro() {
+  return (
+    <section className="flex flex-col container">
+      <h1 className="text-5xl text-center font-bold text-gray-800 my-16">
+        Ops, ocorreu um erro ao exibir esse país!
+      </h1>
+      <Link href="/" className="flex items-center py-2">
+        voltar
+      </Link>
+    </section>
+  )
+}
